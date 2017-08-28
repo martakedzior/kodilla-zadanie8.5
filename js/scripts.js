e364475ee06589a70446a9752be66dd640c6ad32
martakedzior <31068961+martakedzior@users.noreply.github.com>
2017-08-28 19:13:37 +0200

    Zadanie 8.5

---
+++ js/scripts.js
@@ -0,0 +1,13 @@
+// scripts.js
+// Calculating triangle field based on entered values
+
+var a;
+var h;
+
+
+a = prompt ('Enter triangle base');
+h = prompt ('Enter triangle height');
+
+var triangleArea = a * h / 2;
+
+console.log ('Triangle field with base a: ' + a + ' and height h: ' + h + ' is equal to: ' + triangleArea);
